0ce093267fa0337c546968980911e65b74286b93
AlqanbarRami <Rami.qanber@gmail.com>
2021-09-30 10:56:44 +0200

    Added import

--- src/com/company/UnderHouse/Garage.java
+++ src/com/company/UnderHouse/Garage.java
@@ -4,4 +4,15 @@ import com.company.Rooms;
 
 public class Garage extends Rooms {
 
+    public Garage(String typeOfRoom, int totalWindows, String typeOfWindow, String placeOfRoom) {
+        super(typeOfRoom, totalWindows, typeOfWindow, placeOfRoom);
+    }
+
+    public Garage() {
+        this.setTypeOfRoom("Garage");
+        this.setTotalWindows(1);
+        this.setTypeOfWindow("Small");
+        this.setPlaceOfRoom("Under House");
+
+    }
 }
